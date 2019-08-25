d5ba5427b965d11157bc66d77f89048918494b31
tjulinjiayu <linjiayu@tju.edu.cn>
2019-08-25 20:26:04 +0800

    增加输出最优解的组成,注意数组越界问题, i >= 0 会导致i-1越界

--- 01bag.cpp
+++ 01bag.cpp
@@ -2,19 +2,21 @@
 #include <algorithm>
 using namespace std;
 
-int Findmax(int number, int capacity, int *w, int *v)
+void Findwhat(int i, int j, int *w, int *v, int **V, int *item)
 {
-    int **V;
-    V = new int *[number+1];
-    for(int i = 0; i <= number; ++i){
-        V[i] = new int[capacity+1];
+    if(i > 0){
+        if(V[i][j] == V[i-1][j]){
+            item[i] = 0;
+            Findwhat(i-1, j, w, v, V, item);           
+        }
+        else if(j - w[i] >= 0 && V[i][j] == V[i-1][j-w[i]] + v[i]){
+            item[i] = 1;
+            Findwhat(i-1, j-w[i], w, v, V, item);
+        }
     }
-    
-    for(int i = 0; i <= number; ++i)
-        V[i][0] = 0;
-    for(int i = 0; i <= capacity; ++i)
-        V[0][i] = 0;
-
+}
+int Findmax(int number, int capacity, int *w, int *v, int **V)
+{
     int maxval = 0;
     for(int i = 1; i <= number; ++i){
         for(int j = 1; j <= capacity; ++j){
@@ -26,26 +28,43 @@ int Findmax(int number, int capacity, int *w, int *v)
                 maxval = V[i][j];
         }
     }
-    for(int i = 0; i <= number; ++i){
-        delete[] V[i];
-    }
-    delete[] V;
     return maxval;
 }
 int main()
 {
     int number, capacity;
     cin >> number >> capacity;
+
+    int **V;
+    V = new int *[number+1];
+    for(int i = 0; i <= number; ++i){
+        V[i] = new int[capacity+1];
+    }
+    for(int i = 0; i <= number; ++i)
+        V[i][0] = 0;
+    for(int i = 0; i <= capacity; ++i)
+        V[0][i] = 0;
+
     int* w = new int[number+1];
     int* v = new int[number+1];
     for(int i = 1; i <= number; ++i){
         cin >> w[i] >> v[i];
     }
-    int maxvalue = Findmax(number, capacity, w, v);
+    int maxvalue = Findmax(number, capacity, w, v, V);
     cout << "The max value is " << maxvalue << endl;
+    
+    int* item = new int[number+1];
+    Findwhat(number, capacity, w, v, V, item);
 
+    for(int i = 0; i <= number; ++i){
+        if(item[i] == 1)
+            cout << i << " w = " << w[i] << " v = " << v[i] << endl;
+        delete[] V[i];
+    }
+    delete[] V;
     delete[] w;
     delete[] v;
+    delete[] item;
     return 0;
 }
 
